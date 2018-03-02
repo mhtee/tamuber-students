cb9dfda88ac6cab76db234a2f5176adedccb09a0
EC2 Default User <ec2-user@ip-172-31-7-52.us-east-2.compute.internal>
2018-03-02 03:55:11 +0000

    Adds test folders and basic test

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,11 @@
+Given("I am at the home page") do
+  visit "/"
+end
+
+When("I click {string}") do |page_name|
+  click_link page_name
+end
+
+Then("I should see the screen titled {string}") do |title|
+  response.should have_title(title)
+end
