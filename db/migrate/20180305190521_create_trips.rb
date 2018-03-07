dd01a20ab555121194fdf45b40df0ef05d9eebde
mhtee <mohammad_great@hotmail.com>
2018-03-07 19:40:22 +0000

    trip name

--- db/migrate/20180305190521_create_trips.rb
+++ db/migrate/20180305190521_create_trips.rb
@@ -3,6 +3,8 @@ class CreateTrips < ActiveRecord::Migration[5.1]
     create_table :trips do |t|
       t.integer :cart_id
       t.integer :cart_route_id
+      t.references :cart_route, foreign_key: true
+      t.references :cart, foreign_key: true
       t.timestamps
     end
   end
