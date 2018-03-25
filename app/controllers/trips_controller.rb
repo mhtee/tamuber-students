1b43b153a6735cbce09ac37d1368a8ce1aaec25b
Alyssa Lehmann <lehmann.a.m78654@tamu.edu>
2018-03-25 01:19:38 +0000

    route added to trip upon selection

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,8 +1,23 @@
 class TripsController < ApplicationController
     def new
         @cartRoutes = CartRoute.all
+        @cartRoutes.inspect
+        Rails.logger.debug @cartRoutes.inspect
+        @trip = Trip.new
+        @trip.save
     end
     
     def create
+        #logger.debug "#{params}"
+        @trip = Trip.find(params[:trip][:trip_id])
+        @trip.cart_route = CartRoute.find(params[:trip][:cart_route_id])
+        render trips_pickup_path
+    end
+    
+    def pickup
+        @route = Trip.find(params[:trip_id]).cart_route
+        #first coordinate is the start point
+        @start = @route.coordinates[0]
+        
     end
 end
