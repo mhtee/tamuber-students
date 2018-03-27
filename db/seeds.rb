7e5c1bd9c13a1bcbf8b0811f54bbaa65b3b03ce6
Jordan Taylor <jordan.taylor02@gmail.com>
2018-03-26 20:10:39 -0500

    testing new seed route

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-cart_routes = [{:length => 9.99, :startPoint => 'Cyclotron Institute', :endPoint => 'HRBB'}
-  	 ]
+cart_routes = [
+                {:length => 9.99, :startPoint => 'Cyclotron Institute', :endPoint => 'HRBB'},
+                {:length => 9.99, :startPoint => 'MEOB', :endPoint => 'ETB'}
+  	          ]
 
 cart_routes.each do |cart_route|
   CartRoute.create!(cart_route)
 end
 
-coordinates = [{:lat => 30.621284, :lng => -96.340388, :cart_route => CartRoute.find(1)},
-                {:lat => 30.61998, :lng => -96.340193, :cart_route => CartRoute.find(1)}]
+coordinates = [
+                {:lat => 30.621284, :lng => -96.340388, :cart_route => CartRoute.find(1)},
+                {:lat => 30.619980, :lng => -96.340193, :cart_route => CartRoute.find(1)},
+                {:lat => 30.621210, :lng => -96.340388, :cart_route => CartRoute.find(2)},
+                {:lat => 30.623020, :lng => -96.340733, :cart_route => CartRoute.find(2)}
+              ]
 
 coordinates.each do |coordinate|
   Coordinate.create!(coordinate)
