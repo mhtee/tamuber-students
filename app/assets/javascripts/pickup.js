3599138327fc5ee352f65447e42e629dea5dde2c
Caleb <stephcal17@tamu.edu>
2018-04-05 12:42:27 -0500

    added pickup.js and ROS connections

---
+++ app/assets/javascripts/pickup.js
@@ -0,0 +1,25 @@
+function formCheck(form_id) {
+ if (document.getElementById("trip_cart_route_id").value == '') {
+		alert("Please select a route");
+	} else {
+		document.getElementById(form_id).submit();
+	}
+}
+
+window.onload = function() {
+	var ros = new ROSLIB.Ros({
+		url : "ws://166.155.203.130:9090"
+	});
+	console.log(ros);
+	ros.on('connection', function() {
+		console.log("ROS is connected");
+	});
+	ros.on('error', function(error) {
+		console.log("Error connecting to ROS: ", error);
+	});
+	ros.on('close', function() {
+		console.log("Connection to ROS closed");
+	});
+	
+}
+
