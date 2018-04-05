aefbc3ca82124b7a82c8df8cb8d07a9ce8df87d5
Caleb <stephcal17@tamu.edu>
2018-04-05 08:05:17 -0500

    added ROS connection

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -5,3 +5,18 @@ function formCheck(form_id) {
 		document.getElementById(form_id).submit();
 	}
 }
+
+window.onload = function() {
+	var ros = new ROSLIB.Ros({
+		url : "ws://166.155.203.130:9090"
+	});
+	console.log(ros);
+	ros.on('connection', function() {
+		console.log("ROS is connected");
+	});
+	ros.on('error', function(error) {
+		console.log("Error connecting to ROS: ", error);
+	});
+
+}
+
